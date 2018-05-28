48051bbdbdaa34e9b9e63199436c663e84e360c7
ronimb <ronimber@gmail.com>
2018-05-28 10:48:33 +0300

    Added iteration number comparison

--- generate_multi_samples.py
+++ generate_multi_samples.py
@@ -5,9 +5,11 @@ import numpy as np
 import datetime
 import pickle
 # %% Define the conditions
-shift_sizes = [5, 10, 15]
-rates = [15, 50, 100]
-num_neurons = [30, 150, 500]
+shift_sizes = [5]
+rates = [50]
+num_neurons = [30, 500]
+
+num_sets = 30  # Number of sets to generate from each condition
 
 set_sizes = 100 # Shared between conditions
 
@@ -17,30 +19,35 @@ n_conds = len(shift_sizes) * len(rates) * len(num_neurons)
 multi_samples = dict()
 for i, (num_neur, rate, shift) in enumerate(conditions):
     print(f'Now generating {num_neur} neurons at {rate}Hz with {shift}ms temporal shift  -  Condition #{i+1}/{n_conds}')
-    multi_samples[(num_neur, rate, shift)] = make_test_samples(
-    rate=rate,
-    duration_sec=1,
-    num_neur=num_neur,
-    shift_size=shift,
-    set1_size=set_sizes,
-    set2_size=set_sizes)
+    multi_samples[(num_neur, rate, shift)] = []
+    for _ in range(num_sets):
+        multi_samples[(num_neur, rate, shift)].append(make_test_samples(
+        rate=rate,
+        duration_sec=1,
+        num_neur=num_neur ,
+        shift_size=shift,
+        set1_size=set_sizes,
+        set2_size=set_sizes))
 # %% Sample verification
-verfdict = {
-    key: [multi_samples[key]['data'][i][j].shape for i,j
-          in product(
-              range(set_sizes*2),
-              range(key[0]))]
-          for key in multi_samples.keys()
-          }
-obs_mean = pd.Series({key: np.mean(verfdict[key]) for key in verfdict.keys()})
+# verfdict = {
+#     key: [multi_samples[key]['data'][i][j].shape for i,j
+#           in product(
+#               range(set_sizes*2),
+#               range(key[0]))]
+#           for key in multi_samples.keys()
+#           }
+# obs_mean = pd.Series({key: np.mean(verfdict[key]) for key in verfdict.keys()})
 # %% Save data
 date = datetime.datetime.now()
 datestr = f'{date.day:02}{date.month:02}{date.year-2000}'
 with open(rf"D:\Data\Samples_{datestr}.pickle", 'wb') as handle:
     pickle.dump(multi_samples, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-param_str = f'''shift_sizes={shift_sizes}
+param_str = f'''num_neurons={num_neurons}
 rates={rates}
-num_neurons={num_neurons}'''
+shift_sizes={shift_sizes}
+
+num_sets={num_sets}'''
+
 with open(rf"D:\Data\Samples_{datestr}.txt", 'w') as handle:
     handle.write(param_str)
