b288f5e2aabd18071e654c35fa0b7a911ef30b5e
ron <ronimber@gmail.com>
2018-10-03 22:22:10 +0300

    -re-added the conversion module that somehow went missing, now fO rEalZ

---
+++ generation/conversion.py
@@ -0,0 +1,70 @@
+"""
+This is used to convert a stimulus to the format with which brian can work for indexing
+Event times
+"""
+import numpy as np
+from numba import jit, prange
+from multiprocessing import Pool
+
+
+# %%
+@jit(parallel=True)
+def convert_stimulus(stimulus: np.array) -> np.array:
+    """
+
+    :param stimulus: Numpy array where each element is a single neurons spike time, specified in milliseconds
+    :return: converted_stimulus: A structured numpy array with the following fields: index, time, count
+                                 where each item represents events taking place at a 'time' from neuron #'index' in the
+                                 set, and with 'count' representing the number of events that took place at that junction.
+                                 count is required to account for numerous vesicles released in short time intervals
+    """
+    # create placeholder lists
+    indexes = []
+    times = []
+    counts = []
+    for i in prange(stimulus.shape[0]):
+        neuron = np.trunc(stimulus[i] * 10) / 10
+        event_times, event_counts = np.unique(neuron, return_counts=True)
+        indexes.extend([i] * event_times.shape[0])
+        times.extend(event_times)
+        counts.extend(event_counts)
+
+    converted_stimulus = np.array(
+        np.zeros(len(times)),
+        dtype=dict(
+            names=('index', 'time', 'count'),
+            formats=(int, float, int)
+        ))
+    converted_stimulus['index'] = indexes
+    converted_stimulus['time'] = times
+    converted_stimulus['count'] = counts
+    return converted_stimulus
+
+
+def convert_stimuli_set(stimuli_set: np.array,
+                        pool_size: int=8):
+    """
+    neuron index consider both neuron number and stimulus number
+    :param stimuli_set:
+    :param pool_size:
+    :return:
+    """
+    num_neurons = stimuli_set['stimulus'][0].shape[0]
+
+    labels = stimuli_set['label']
+    with Pool(pool_size) as p:
+        res = p.map(convert_stimulus, stimuli_set['stimulus'])
+        p.close()
+        p.join()
+    ts = np.hstack(
+        [
+            [x['index'] + num_neurons * i, x['time'], x['count']]
+            for i, x in enumerate(res)]
+    )
+    converted_samples = np.zeros(ts.shape[1],
+                                 dtype={'names': ('index', 'time', 'count'),
+                                        'formats': (int, float, int)})
+    converted_samples['index'] = ts[0]
+    converted_samples['time'] = ts[1]
+    converted_samples['count'] = ts[2]
+    return converted_samples, labels
