52619860dcd7c34cb5f0ae45631ec801d3448ba7
ron <ronimber@gmail.com>
2018-10-09 22:06:20 +0300

    minor changes

--- tools/save_obj.py
+++
@@ -1,6 +0,0 @@
-import pickle
-import dill
-
-def save_obj(obj, location, save_module=pickle):
-    with open(location, 'wb') as file:
-        pickle.dump(obj, location, protocol=pickle.HIGHEST_PROTOCOL)
