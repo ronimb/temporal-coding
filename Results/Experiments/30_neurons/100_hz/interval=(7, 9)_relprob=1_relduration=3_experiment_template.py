7e692319433d2af3ff99e46918800719ea27ae48
ronimber <ronimber@gmail.com>
2018-10-12 00:59:49 +0000

    Sync to laptop

--- Results/Experiments/30_neurons/100_hz/interval=(7, 9)_relprob=1_relduration=3_experiment_template.py
+++ Results/Experiments/30_neurons/100_hz/interval=(7, 9)_relprob=1_relduration=3_experiment_template.py
@@ -11,7 +11,7 @@ start_date = gen_datestr()
 print(f"---- Started experiment : {start_date}")
 # %%
 # Determine name for save folder
-report_folder = '/home/ron/OneDrive/Documents/Masters/Parnas/temporal-coding/Results/Experiments/30_neurons/100_hz'
+report_folder = '~/PycharmProjects/temporal-coding/Results/Experiments/30_neurons/100_hz'
 # Make sure the folder exists, create with all subfolders if it does not
 check_folder(report_folder)
 # Name of files from current condition
