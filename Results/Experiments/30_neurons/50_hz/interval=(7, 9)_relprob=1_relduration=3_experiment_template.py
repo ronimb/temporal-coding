2db38ecf393568c1284b4c9ad6c96eff94a1dd97
ronimber <ronimber@gmail.com>
2018-10-13 12:51:08 +0000

    Sync to laptop

--- Results/Experiments/30_neurons/50_hz/interval=(7, 9)_relprob=1_relduration=3_experiment_template.py
+++ Results/Experiments/30_neurons/50_hz/interval=(7, 9)_relprob=1_relduration=3_experiment_template.py
@@ -11,7 +11,7 @@ start_date = gen_datestr()
 print(f"---- Started experiment : {start_date}")
 # %%
 # Determine name for save folder
-report_folder = '~/PycharmProjects/temporal-coding/Results/Experiments/30_neurons/50_hz'
+report_folder = '/home/ronimber/PycharmProjects/temporal-coding/Results/Experiments/30_neurons/50_hz'
 # Make sure the folder exists, create with all subfolders if it does not
 check_folder(report_folder)
 # Name of files from current condition
@@ -40,7 +40,7 @@ training_params = dict(
 # Changes to this might be required for different modes of generation (e.g. comparing different frequencies)
 stimuli_creation_params = dict(
     number_of_neurons=30,
-    frequency=50,
+    frequency=15,
     stimulus_duration=stimulus_duration,
     set_size=set_size
 )
